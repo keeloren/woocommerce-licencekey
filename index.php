2883154f9c6c44a8b9623b7a1f5baf3ec2b3721f
Nguyen Duy Nguyen <nguyennd@greenglobal.vn>
2021-02-01 17:58:37 +0700

    Update CORS for request API

--- index.php
+++ index.php
@@ -13,5 +13,10 @@
  */
 define( 'WP_USE_THEMES', true );
 
+header ("Access-Control-Allow-Origin: *");
+header ("Access-Control-Expose-Headers: Content-Length, X-JSON");
+header ("Access-Control-Allow-Methods: GET, POST, PATCH, PUT, DELETE, OPTIONS");
+header ("Access-Control-Allow-Headers: *");
+
 /** Loads the WordPress Environment and Template */
 require __DIR__ . '/wp-blog-header.php';
